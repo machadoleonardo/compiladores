e56bba2b6a11a3f517f08f4367c4db0e97d59a4d
Leonardo Martins Machado <leonardo.machado@programmer.net>
2017-05-10 20:22:45 -0300

    arquivo teste correto incompleto

---
+++ testeLexico_correto.java
@@ -0,0 +1,21 @@
+class TesteLexico extends SuperTesteLexico {
+	/*
+	Metodo para verificar se o numero eh par
+	*/
+	boolean verificaSePar(int i){
+		boolean par= false;
+		
+		if(i % 2 == 0){
+			par=true;
+		}
+		return par;
+	}
+	
+	
+	
+
+}
+
+class test2 {
+
+}
