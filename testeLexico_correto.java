488a756a550f2071eaab69342c07e8fe652ab0f0
Leonardo Martins Machado <leonardo.machado@programmer.net>
2017-05-10 23:45:45 -0300

    arquivo teste correto

--- testeLexico_correto.java
+++ testeLexico_correto.java
@@ -1,4 +1,4 @@
-class TesteLexico extends SuperTesteLexico {
+class TesteLexico {
 	/*
 	Metodo para verificar se o numero eh par
 	*/
@@ -11,11 +11,23 @@ class TesteLexico extends SuperTesteLexico {
 		return par;
 	}
 	
-	
-	
-
 }
 
-class test2 {
+
+class TexteLexico_2 extends TesteLexico {
+	
+	string verificaSeMaiorQue18(int idade){
+		if (idade >=0){
+			string maior_18 = "maior de idade";
+			string menor_18 = "menor de idade";
+			case (idade >= 18):	
+				return maior_18;
+			default:
+				return menor_18;
+		}else{
+			return "idade invalida";
+		}
+		
+	}
 
 }
